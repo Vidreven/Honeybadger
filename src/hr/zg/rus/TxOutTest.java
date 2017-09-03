1f86c453b6609005260ea0f162788b4881a7d3ae
Vidreven <vidreven@yahoo.com>
2017-09-03 17:28:34 +0200

    Correct output serialization.

--- src/hr/zg/rus/TxOutTest.java
+++ src/hr/zg/rus/TxOutTest.java
@@ -7,6 +7,7 @@ public class TxOutTest {
     public static void main(String[] args) {
         int amount;
         String script;
+        String scriptPubKey = "76a91496c8fe3196453eb48aaad26d5e1b96a8681e93c188ac";
 
         TxOut out0 = new TxOut();
 
@@ -35,13 +36,14 @@ public class TxOutTest {
             System.out.println("Invalid script!");
         }
 
-        out0.setScriptPubKey("76a91496c8fe3196453eb48aaad26d5e1b96a8681e93c188ac");
+        out0.setScriptPubKey(scriptPubKey);
 
         if (out0.getScriptPubKey() == null){
             System.out.println("Script should not be empty after setting!");
         }
 
-        TxOut out1 = new TxOut("76a91496c8fe3196453eb48aaad26d5e1b96a8681e93c188ac");
+        TxOut out1 = new TxOut(scriptPubKey);
+        String out1_serialized = "220200000000000019" + scriptPubKey;
 
         if(out1.getScriptPubKey() == null){
             System.out.println("Script should not be empty!");
@@ -49,14 +51,21 @@ public class TxOutTest {
         if(out1.getAmount() < 546){
             System.out.println("Amount should be at leas 546 satoshi!");
         }
+        if(!out1.toString().equals(out1_serialized)){
+            System.out.println("Wrong output serialization!");
+        }
 
-        TxOut out2 = new TxOut(100_000, "76a91496c8fe3196453eb48aaad26d5e1b96a8681e93c188ac");
+        TxOut out2 = new TxOut(100_000, scriptPubKey);
+        String out2_serialized = "a08601000000000019" + scriptPubKey;
 
         if(out2.getScriptPubKey() == null){
             System.out.println("Script should not be empty!");
         }
         if(out2.getAmount() < 546){
             System.out.println("Amount should be at leas 546 satoshi!");
+        }
+        if(!out2.toString().equals(out2_serialized)){
+            System.out.println("Wrong output serialization!");
         }
     }
 }
